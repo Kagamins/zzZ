51570a52c32e5a589af4ddc71b63dc37f2455bdc
Kagamins <nsoulper@gmail.com>
2022-06-15 17:23:34 +0300

    all in good standard

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,17 @@
 import styles from './Footer.module.css'
-
+import { Button } from 'reactstrap'
+import React from "react";
 export default function Footer () {
     return ( <footer className={styles.footer}>
+                
+        <a className="btn btn-primary"   href="https://twitter.com/zzzfreecompany">
+  
+        Twitter
+        </a>
+        <a className="btn btn-danger"  href="https://www.instagram.com/zzzfreecompany/">
 
+        Instagram
+      </a>
+        
             </footer>)
 }
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,11 @@
 import Image from 'next/image'
 import Head from 'next/head';
 import styles from '../styles/Home.module.css'
+import React from "react";
+import { useState } from 'react';
+// reactstrap components
+import { Button, Card, Collapse,CardBody , Row , Col } from "reactstrap";
+
 export async function getServerSideProps  (context){
    
     const jsondata = await fetch('https://xivapi.com/freecompany/9237023573225242696');
@@ -8,6 +13,8 @@ export async function getServerSideProps  (context){
     return ({ props : { data } })
 }
 function members({data }){
+    const [collapseOpen, setCollapseOpen] = useState(false);
+    const [collapseOpenSeek, setCollapseOpenSeek] = useState(false);
 
     
     
@@ -26,122 +33,127 @@ function members({data }){
                     <Head>
                     <title>About zzZ Fc</title>
                     </Head>
-                    <main className="mb-3 p-5">
- <table className="table table-dark table-striped table-hover  ">
-       <thead>
-           <tr>
-                <td className='text-center'>Free Company Name</td>
-                <td className='text-center'><span className="material-symbols-outlined">home</span></td>
-                <td className='text-center'><span className="material-symbols-outlined">home</span></td>
-           </tr>
-       </thead>
-        <tbody>
-        <tr>
-        <td className="text-center">{data.FreeCompany.Name}</td>
-        <td className="text-center">{data.FreeCompany.Estate.Plot}</td>
-        <td className="text-center">{data.FreeCompany.Estate.Name}</td>
-        </tr>
-        </tbody>
-        </table>
+                    <main className="mt-4 p-5 bg-dark  rounded">
+ <div className="mt-4 p-5 bg-primary text-white rounded  ">
+           
+           <Row>
+                <Col>Free Company Name</Col>
+                <Col><span className="material-symbols-outlined">home</span></Col>
+           </Row>
+        
+        <div className=" row mt-4 p-5 bg-success text-white rounded ">
+        <Col>{data.FreeCompany.Name}</Col>
+        <Col>{data.FreeCompany.Estate.Plot}</Col>
+        <Col>{data.FreeCompany.Estate.Name}</Col>
+        </div>
+        </div>
+        <div className='row gap-2'>
+        <div className=" col mt-4 p-5 bg-light text-black rounded ">
 
-        <table className="table table-dark table-striped table-hover ">
-        <thead>
-            <tr>
-                <th className="text-center">Active Members </th>
-                <th className="text-center">Rank</th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-        <td className="text-center">{data.FreeCompany.ActiveMemberCount}</td>
-        <td className="text-center">{data.FreeCompany.Rank}</td>
+            <Row>
+                <Col >Active Members </Col>
+                <Col >Rank</Col>
+            </Row>
 
-        </tr>
-        </tbody>
-        </table>
-        <table className="table table-dark table-striped table-hover ">
-        <thead>
-            <tr>
+        <Row>
+        <Col >{data.FreeCompany.ActiveMemberCount}</Col>
+        <Col >{data.FreeCompany.Rank}</Col>
+        </Row>
 
-                <th className="text-center">Monthly Standing</th>
-                <th className="text-center">Weekly Standing</th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
+        </div>
 
-        <td className="text-center">{data.FreeCompany.Ranking.Monthly}</td>
-        <td className="text-center">{data.FreeCompany.Ranking.Weekly}</td>
-        </tr>
-        </tbody>
-        </table>
+        <div className=" col mt-4 p-5 bg-light text-black rounded ">
+            
+            <Row>
+
+                <Col  className="col">Monthly Standing</Col>
+                <Col  className="col">Weekly Standing</Col>
+            </Row>
+
+        <Row>
+
+        <Col  className="col">{data.FreeCompany.Ranking.Monthly}</Col>
+        <Col  className="col">{data.FreeCompany.Ranking.Weekly}</Col>
+        </Row>
+        
+        </div>
+        </div>
+    <Row>
+    <Col>
+        <Card  className=" row mt-4 p-5 bg-dark  rounded  ">
+        <Button
+          color="primary"
+          href="#Reputation"
+          onClick={() => {
+            setCollapseOpen(!collapseOpen);
+          }}
+        >Reputation</Button>
+
+        
+            <Collapse isOpen={collapseOpen} aria-labelledby="Rep" id="Reputation">
+
+                    <CardBody className=" row mt-4 p-5 bg-primary text-white  rounded  ">
+
+                    <Row>
+                    {data.FreeCompany.Reputation.map(rep=><Col xs="auto" key={rep.Name} className="col">{rep.Name}  :  {rep.Rank} </Col>)}
+                    </Row>
+
+                    </CardBody>
+
+            </Collapse>
+        </Card>
+    </Col>
+    <Col>
+        <Card  className=" row mt-4 p-5 bg-dark  rounded  ">
+        <Button
+          color="danger"
+          href="#Seek"
+          onClick={() => {
+            setCollapseOpenSeek(!collapseOpenSeek);
+          }}
+        >Focus!!</Button>
+
+        
+            <Collapse isOpen={collapseOpenSeek} aria-labelledby="Rep" id="Seek">
+
+                    <CardBody>
+        <div className=" row mt-4 p-5 bg-danger text-white rounded ">
 
 
-        <table  className="table table-dark table-striped table-hover  ">
-        <thead>
-            <tr>
-            {data.FreeCompany.Reputation.map(rep=><th key={rep.Name} className="text-center">{rep.Name}</th>)}
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-        {data.FreeCompany.Reputation.map(rep=><td key={rep.Name} className="text-center">{rep.Rank}</td>)}
-        </tr>
-        </tbody>
-        </table>
-        <table className="table table-dark table-striped table-hover ">
-        <thead>
-        <tr>
-        {firstrowseeker.map(seek=><td key={seek.Name} className="text-center">{seek.Name}</td>)}
-</tr>
-</thead>
-<tbody>
-    <tr>
-    {firstrowseeker.map(seek=><td key={seek.Name} className="text-center"><Image alt=' ' className="text-center"  width={32} height={32}  src={seek.Icon}></Image></td>)}
+    <Row>
+    {firstrowseeker.map(seek=><Col  key={seek.Name} className="col"><Image alt={seek.Name} className="col"  width={32} height={32}  src={seek.Icon}></Image> {seek.Name} </Col>)}
 
-    </tr>
-</tbody>
-</table>
-<table className="table table-dark table-striped table-hover  ">
-        <thead>
-        <tr>
-        {secondrowseeker.map(seek=><td key={seek.Name} className="text-center">{seek.Name}</td>)}
-</tr>
-</thead>
-<tbody>
-    <tr>
-    {secondrowseeker.map(seek=><td key={seek.Name} className="text-center"><Image className="text-center" alt=' '  width={32} height={32} src={seek.Icon}></Image></td>)}
+    </Row>
 
-    </tr>
-</tbody>
-</table>
 
-<table className="table table-dark table-striped table-hover  ">
-        <thead>
-        <tr>
-        {thirdrowseeker.map(seek=><td key={seek.Name} className="text-center">{seek.Name}</td>)}
-</tr>
-</thead>
-<tbody>
-    <tr>
-    {thirdrowseeker.map(seek=><td key={seek.Name} className="text-center"><Image className="text-center" alt=' ' width={32} height={32}  src={seek.Icon}></Image></td>)}
 
-    </tr>
-</tbody>
-</table>
+    <Row>
+    {secondrowseeker.map(seek=><Col  key={seek.Name} className="col"><Image className="col" alt={seek.Name}  width={32} height={32} src={seek.Icon}></Image> {seek.Name}</Col>)}
 
-<table className="table table-dark table-striped table-hover    ">
-        <thead>
-        <tr>
-        {data.FreeCompany.Seeking.map(seek=><td key={seek.Name} className="text-center">{seek.Name}</td>)}
-</tr>
-</thead>
-<tbody>
-    <tr>
-    {data.FreeCompany.Seeking.map(seek=><td key={seek.Name} className="text-center"><Image className="text-center" alt='' width={32} height={32} src={seek.Icon}></Image></td>)}
-    </tr>
-</tbody>
-</table>
+    </Row>
+
+
+
+    <Row>
+    {thirdrowseeker.map(seek=><Col  key={seek.Name} className="col"><Image className="col" alt={seek.Name} width={32} height={32}  src={seek.Icon}></Image> {seek.Name}</Col>)}
+
+    </Row>
+</div>
+</CardBody>
+</Collapse>
+</Card>
+</Col>
+</Row>
+<h1 className='text-white text-center'> Seeking </h1>
+<div className=" row mt-4 p-5 bg-secondary text-white rounded    ">
+        <Row>
+        {data.FreeCompany.Seeking.map(seek=><Col  key={seek.Name} className="col">{seek.Name}</Col>)}
+</Row>
+
+    <Row>
+    {data.FreeCompany.Seeking.map(seek=><Col key={seek.Name} className="col"><Image className="col" alt='' width={32} height={32} src={seek.Icon}></Image></Col>)}
+    </Row>
+</div>
 </main>
         </div>
     )
